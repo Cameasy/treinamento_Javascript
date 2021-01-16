bcdca15bcaa6b8c816cddc5e1f5965e9ab4aa975
Cameasy <camila.correia662@gmail.com>
2021-01-16 19:47:34 -0300

    Exercicio 1 do treinamento Jedi. Alterando background-color através de butões com JS

---
+++ lista1/script.js
@@ -0,0 +1,10 @@
+function mudar2(color) {
+    document.body.className = "clorchangered";
+ }
+ 
+ window.addEventListener("load",function() { changeBackground('red') });
+ function mudar(color) {
+    document.body.className = "clorchangegreen";
+ }
+ 
+ window.addEventListener("load",function() { changeBackground('green') });
